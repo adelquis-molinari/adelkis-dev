4bdc555b05ebce515062519a56390ef922b7ed17
adelquis-molinari <66643930+adelquis-molinari@users.noreply.github.com>
2022-06-20 23:27:02 -0300

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,29 +1,24 @@
 import styles from "./Footer.module.css";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 function Footer() {
   const year = new Date().getFullYear();
-  const [isDark, setIsDarck] = useState(false);
+  // const [isDark, setIsDarck] = useState(false);
   // dark mode existe in the browser
-  useEffect(() => {
-    const isDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    if (isDarkMode) {
-      setIsDarck(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const isDarkMode = window.matchMedia(
+  //     "(prefers-color-scheme: dark)"
+  //   ).matches;
+  //   if (isDarkMode) {
+  //     setIsDarck(true);
+  //   }
+  // }, []);
   return (
     <footer className={styles.footer}>
       <a href="/" rel="noopener noreferrer">
         {year} © Powered by{" "}
         <span className={styles.logo}>
-          <Image
-            src={isDark ? "/img/logo-dark.png" : "/img/logo.png"}
-            alt="Adelkis.dev"
-            width={88}
-            height={16}
-          />
+          <Image src="/img/logo.png" alt="Adelkis.dev" width={88} height={16} />
         </span>
       </a>
     </footer>
