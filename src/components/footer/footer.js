9e76be95d8c6c529abd43619a3fb77df94ad4cd4
adelquis-molinari <66643930+adelquis-molinari@users.noreply.github.com>
2022-06-21 09:11:39 -0300

    cambios en index

---
+++ src/components/footer/footer.js
@@ -0,0 +1,32 @@
+import styles from "./Footer.module.css";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+function Footer() {
+  const year = new Date().getFullYear();
+  const [isDark, setIsDarck] = useState(false);
+  useEffect(() => {
+    const isDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (isDarkMode) {
+      setIsDarck(true);
+    }
+  }, []);
+  return (
+    <footer className={styles.footer}>
+      <a href="/" rel="noopener noreferrer">
+        {year} © Powered by{" "}
+        <span className={styles.logo}>
+          <Image
+            src={isDark ? "/logo-dark.png" : "/logo.png"}
+            alt="Adelkis.dev"
+            width={88}
+            height={16}
+          />
+        </span>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
