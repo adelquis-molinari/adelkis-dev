694fa8905894c5a0757d39a0cd42066b125dc58b
adelquis-molinari <adelkis003@gmail.com>
2022-09-15 09:55:57 -0300

    refeatured card article

---
+++ src/helpers/data-hero.js
@@ -0,0 +1,42 @@
+export const links = [
+  {
+    className:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+    classNameMenuDropdown:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+    href: "/",
+    label: "Home",
+  },
+  {
+    className:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+    classNameMenuDropdown:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+    href: "/blog",
+    label: "Blog",
+  },
+  {
+    className:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+    classNameMenuDropdown:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+    href: "/about",
+    label: "About",
+  },
+  {
+    className:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+    classNameMenuDropdown:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+    href: "https://twitter.com/adelkisDev",
+    label: "Twitter",
+  },
+  {
+    className:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+    classNameMenuDropdown:
+      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+    href: "https://github.com/adelquis-molinari",
+    label: "Github",
+  },
+];
