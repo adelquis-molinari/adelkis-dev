36e4488274372b73037c8a77605a4a1adc9f5af3
adelquis-molinari <adelkis003@gmail.com>
2022-11-19 17:55:55 -0300

    change section preview proyect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,73 +1,73 @@
 import HeaderLink from "./HeaderLink.jsx";
 import { links } from "../helpers/data-hero";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Navbar() {
-
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   const toggleMenu = () => {
     setShow(!show);
-  }
+  };
 
   return (
-    <nav className="bg-white dark:bg-gray-800 shadow ">
-    <div className="container mx-auto">
-      <div className="flex items-center justify-between h-16">
-        <div className="w-full justify-between flex items-center">
-          <a className="flex-shrink-0" href="/">
-            <img className="h-8" src="/adelkis-logo.png" alt="adelkis.dev" />
-          </a>
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              {
-                links.map((link) => (
-                  <HeaderLink classType={link.classType} href={link.href} label={link.label}/>
-                ))
-              }
+    <nav className="fixed top-0 md:relative w-full z-10 bg-zinc-900 ">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between h-16">
+          <div className="w-full justify-between flex items-center">
+            <a className="flex-shrink-0" href="/">
+              <img className="h-8" src="/adelkis-logo.png" alt="adelkis.dev" />
+            </a>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                {links.map((link) => (
+                  <HeaderLink
+                    key={link.id}
+                    classType={link.classType}
+                    href={link.href}
+                    label={link.label}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="block">
-          <div className="ml-4 flex items-center md:ml-6"></div>
-        </div>
-        <div className="-mr-2 flex md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
-          >
-            <svg
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="h-8 w-8"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
+          <div className="block">
+            <div className="ml-4 flex items-center md:ml-6"></div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-gray-400 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
             >
-              <path
-                d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="h-8 w-8"
+                viewBox="0 0 1792 1792"
+                xmlns="http://www.w3.org/2000/svg"
               >
-              </path>
-            </svg>
-          </button>
+                <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    {
-      show ? (<div className="md:hidden">
-      <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 absolute bg-slate-200
-       -top-28 left-0 w-max max-h-max ">
-        {
-          links.map((link) => (
-            <HeaderLink classType={link.classTypeMenuDropdown} href={link.href} label={link.label} />
-          ))
-        }
-      </div>
-    </div>) : null
-    }
-    
-  </nav>
-  )
+      {show ? (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 ">
+            {links.map((link) => (
+              <HeaderLink
+                key={link.id}
+                classType={link.classTypeMenuDropdown}
+                href={link.href}
+                label={link.label}
+              />
+            ))}
+          </div>
+        </div>
+      ) : null}
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/helpers/data-hero.js
+++ src/helpers/data-hero.js
@@ -1,42 +1,29 @@
 export const links = [
   {
+    id: 1,
     classType:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
     classTypeMenuDropdown:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
     href: "/",
     label: "Home",
   },
   {
+    id: 2,
     classType:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
     classTypeMenuDropdown:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
     href: "/blog",
     label: "Blog",
   },
   {
+    id: 3,
     classType:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
     classTypeMenuDropdown:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
     href: "/about",
     label: "About",
-  },
-  {
-    classType:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
-    classTypeMenuDropdown:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
-    href: "https://twitter.com/adelkisDev",
-    label: "Twitter",
-  },
-  {
-    classType:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
-    classTypeMenuDropdown:
-      "text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium",
-    href: "https://github.com/adelquis-molinari",
-    label: "Github",
   },
 ];
