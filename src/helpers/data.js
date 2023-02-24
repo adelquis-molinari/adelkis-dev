5b841c7e853befecf3ecae7b020f5f3f31fda682
adelquis-molinari <Adelkis003@gmail.com>
2023-02-24 16:31:34 -0300

    change and fix

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -2,9 +2,9 @@ export const links = [
   {
     id: 1,
     classType:
-      "text-gray-300 hover:text-cyan-400 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white py-2 rounded-md text-sm font-medium",
     classTypeMenuDropdown:
-      "text-gray-300 hover:text-cyan-400 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium",
+      "text-gray-300 hover:text-cyan-400 dark:hover:text-white py-2 rounded-md text-sm font-medium",
     href: "/",
     label: "Home",
   },
